901dfb44bccd3ab54fc4c93fee3a2f3982e4ea79
ci <ci@josephearl.co.uk>
2024-02-29 11:53:41 +0000

    Update documentation

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"uk.co.josephearl.result","l":"Result.Failure"},{"p":"uk.co.josephearl.result","l":"Result"},{"p":"uk.co.josephearl.result","l":"Result.Success"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.josephearl.result","l":"Result.Failure"},{"p":"io.github.josephearl.result","l":"Result"},{"p":"io.github.josephearl.result","l":"Result.Success"}];updateSearchResults();
